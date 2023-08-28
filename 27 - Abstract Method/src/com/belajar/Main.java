cc9310ddf46c7440c01cb1f352c7d23a4b166730
00-mikazuki <diojuan432@gmail.com>
2023-08-28 20:06:59 +0700

    materi 27 - Abstract Method

--- 27 - Abstract Method/src/com/belajar/Main.java
+++ 27 - Abstract Method/src/com/belajar/Main.java
@@ -1,8 +1,8 @@
 package com.belajar;
 
-// import com.hero.Hero; // tidak perlu karena tidak dapat dipakai
 import com.hero.HeroIntel;
 import com.hero.HeroAgility;
+import com.hero.HeroStrength;
 
 public class Main {
   public static void main(String[] args) {
@@ -13,8 +13,14 @@ public class Main {
     HeroAgility hero2 = new HeroAgility("Mario");
     hero2.display();
 
-    // membuat object dari kelas abstract
-    // Hero hero3 = new Hero("Mario"); // tidak bisa
-    // hero3.display();
+    hero1.levelUp();
+    hero2.levelUp();
+
+    hero1.display();
+    hero2.display();
+
+    HeroStrength hero3 = new HeroStrength("Ucup");
+    hero3.levelUp();
+    hero3.display();
   }
 }
